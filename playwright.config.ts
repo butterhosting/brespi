3908c2268b0d15ecaa183bdb8c18d1004d1d67b4
Jessy <contact@moysus.com>
2026-02-25 21:44:16 +0100

    commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -43,7 +43,12 @@ export default defineConfig({
 
   /* Build and start the production image before running tests */
   webServer: {
-    command: "./brespi.sh image create --postgresql --mariadb --stage e2e && docker compose -f compose-e2e.yaml up",
+    command: [
+      "./brespi.sh image create --postgresql --mariadb --stage e2e",
+      process.env.CI
+        ? `HOST_UID=${process.getuid!()} HOST_GID=${process.getgid!()} docker compose -f compose-e2e.yaml up`
+        : "docker compose -f compose-e2e.yaml up",
+    ].join(" && "),
     stdout: "pipe",
     url: "http://localhost:3000",
     reuseExistingServer: !process.env.CI,
